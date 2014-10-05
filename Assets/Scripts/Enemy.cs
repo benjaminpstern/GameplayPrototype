3bed6658d73863dae33e23fa786330767b820708
Shiying <szheng@oberlin.edu>
2014-10-05 15:44:43 -0400

    Add enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Enemy : MonoBehaviour {
+
+	public float baseSpeed;
+	public float speedMod;
+	public float aggroRadius;
+	public Vector3 playerLocation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
