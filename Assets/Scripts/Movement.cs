7862fdbaa8ac45dbf026a90daa910a128dfba4e6
PeterAguiar <paguiar@oberlin.edu>
2014-10-06 23:42:42 -0400

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,16 @@
+using UnityEngine;
+using System.Collections;
 protected float speed;
  	public float tilt = 1.0f;
  	public float slowTime = 3f;
--	public Shader invis; 
--	public Shader visible;
-+ public class Movement : Splodeable {
+	public Shader invis; 
+	public Shader visible;
+ public class Movement : Splodeable {
  	float timer;
  	void Start(){
--		invis  = Shader.Find ("Transparent/Diffuse");
--		visible = Shader.Find ("Sprites/Default");
-+	
+		invis  = Shader.Find ("Transparent/Diffuse");
+		visible = Shader.Find ("Sprites/Default");
+	
  		speed = baseSpeed;
  		timer = 0;
  	}
@@ -16,16 +18,16 @@ protected float speed;
  		if(Input.GetKey(KeyCode.Q)){
  			transform.Rotate (new Vector3(0,2,0));
  		}
--		if (Input.GetKey (KeyCode.Space)) {
--			//if(renderer.material.shader != invis){
--					renderer.material.shader = invis;
--					renderer.material.color = new Color(1,1,1,.5f);
--		}if(Input.GetKey (KeyCode.Mouse0)){
--			
--					renderer.material.shader = visible;
--					renderer.material.color = new Color(1,1,1,1);
--				}
-+	
+		if (Input.GetKey (KeyCode.Space)) {
+			//if(renderer.material.shader != invis){
+					renderer.material.shader = invis;
+					renderer.material.color = new Color(1,1,1,.5f);
+		}if(Input.GetKey (KeyCode.Mouse0)){
+			
+					renderer.material.shader = visible;
+					renderer.material.color = new Color(1,1,1,1);
+				}
+	
  		transform.rotation = Quaternion.Euler(0,0,0);
  		if(timer > 0){
  			timer -= Time.deltaTime;
