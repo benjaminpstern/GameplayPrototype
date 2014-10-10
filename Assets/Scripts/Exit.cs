43490b01d9b106ebb9c04d8f3a400e9a3f1a88b9
PeterAguiar <paguiar@oberlin.edu>
2014-10-09 20:45:36 -0400

    Update Exit.cs
    Made the trigger for tags which is a convention and takes one less line of code. 
    
    Deleted the "you win", the wait for seconds only works in an IEnumerate functions which has its own syntax. 
    
    Added a public string that allows us to change the next level we want to load, making this script use able in all of our scenes.

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,14 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Exit : MonoBehaviour {
-	public GameObject player;
-	public GUIText gui_text;
+	public string nextLevel;
+
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject == player.gameObject){
-			gui_text.text = "You win!";
-			WaitForSeconds wait = new WaitForSeconds(2);
-			Application.LoadLevel ("Scene2");
+		if (other.tag == "Player"){
+			Application.LoadLevel (nextLevel);
 		}
 	}
 }
