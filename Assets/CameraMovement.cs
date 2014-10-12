f7ecced1fa22c6fc8ba99c67039c6e0395e31754
Shiying <szheng@oberlin.edu>
2014-10-12 00:41:22 -0400

    Make camera frame size adjustable.

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 	public GameObject player;
 	private Vector3 offset;
+	public float frameSize;
 
 	// Use this for initialization
 	void Start () {
 		offset = transform.position;
+		camera.orthographicSize = frameSize;
 	}
 	
 	// Update is called once per frame
