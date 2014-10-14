efdeaf2b916fa74f25fb2a7d40e23778d0e85c18
benjaminpstern <bstern@oberlin.edu>
2014-10-14 02:23:53 -0400

    cleaning up some code

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,7 @@ using System.Collections;
 public class Movement : Splodeable {
 	public float baseSpeed = 2.0f;
 	protected float speed;
-	public float tilt = 1.0f;
 	public float slowTime = 3f;
-	public GameObject exit;
 	void Start(){
 		speed = baseSpeed;
 	}
@@ -17,10 +15,6 @@ public class Movement : Splodeable {
 		rigidbody2D.velocity = movement * speed;
 		
 		transform.rotation = Quaternion.Euler(0,0,0);
-		
-		if (Vector3.Distance(transform.position, exit.transform.position) < 0.1){
-			Application.LoadLevel ("Scene2");
-		}
 	}
 	public override void explode(){
 		lose();
