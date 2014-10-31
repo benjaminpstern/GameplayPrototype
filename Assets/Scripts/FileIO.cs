edc3114fc83f8d28ad65c8f0190619ce522d80a3
Shiying <szheng@oberlin.edu>
2014-10-31 17:49:59 -0400

    Started making level editor. Created FileIO class.

---
+++ Assets/Scripts/FileIO.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class FileIO {
+
+	public int[][] GetTilesFromFile(string fileName){
+		TextAsset txt = (TextAsset) Resources.Load ("LevelFiles/" + fileName, typeof (TextAsset));
+		string content = txt.text;
+		List<string> lines = new List<string> (content.Split ('\n'));
+		int[][] tiles = new int[lines.Count][];
+		for (int i = 0; i < lines.Count; i++){
+			string[] line = lines[i].Split(' ');
+			tiles[i] = new int[line.Length];
+			for (int j = 0; j < line.Length; j++){
+				tiles[i][j] = int.Parse(line[j]);
+			}
+		}
+		return tiles;
+	}
+
+	public void WriteTilesToFile(int[][] tiles, string fileName){
+		string path = "Assets/Resources/LevelFiles/" + fileName + ".txt";
+		StreamWriter sw;
+		if (File.Exists(path)) {
+			sw = new StreamWriter(path);
+		}
+		else {
+			sw = File.CreateText(path);
+		}
+
+		for (int i = 0; i < tiles.Length; i++){
+			string line = "";
+			for (int j = 0; j < tiles[i].Length; j++){
+				line += tiles[i][j].ToString();
+				if (j < tiles[i].Length - 1) line += " ";
+			}
+			sw.WriteLine(line);
+		}
+		sw.Close();
+	}
+
+}
---
+++ Assets/Scripts/LevelEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class LevelEditor : MonoBehaviour {
+	private int[][] tiles;
+	private FileIO io;
+	public string inputFile;
+	public string outputFile;
+
+	// Use this for initialization
+	void Start () {
+		io = new FileIO();
+		LoadFile(inputFile);
+		WriteFile(outputFile);
+	}
+	
+	void LoadFile(string fileName){
+		string path = "Assets/Resources/LevelFiles/" + fileName + ".txt";
+		if (fileName != "" && File.Exists(path)) {
+			tiles = io.GetTilesFromFile(fileName);
+		}
+	}
+
+	void WriteFile(string fileName){
+		if (fileName != "") {
+			io.WriteTilesToFile(tiles, fileName);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
