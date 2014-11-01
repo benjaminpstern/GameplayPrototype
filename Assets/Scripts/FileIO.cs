bfb7fa76e7ee31e853f45103680e9bd12ef19210
Shiying <szheng@oberlin.edu>
2014-10-31 20:28:30 -0400

    Added things to fileIO

--- Assets/Scripts/FileIO.cs
+++ Assets/Scripts/FileIO.cs
@@ -1,26 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.IO;
 
-public class FileIO {
+/*
+ * The format of a level file should be:
+ * TILES
+ * 1 1 1 1 1 1 1
+ * 1 0 0 1 0 0 1
+ * 1 0 0 0 0 0 1
+ * 1 0 0 1 1 1 1
+ * PLAYER
+ * (0,0,0)
+ * FAST ENEMY
+ * (3,1,5) (2,4,7) (9,1,3)
+ * ...
+ */
 
-	public int[][] GetTilesFromFile(string fileName){
+public class FileIO : MonoBehaviour{
+
+	public int[][] ReadFromFile(string fileName){
 		TextAsset txt = (TextAsset) Resources.Load ("LevelFiles/" + fileName, typeof (TextAsset));
 		string content = txt.text;
-		List<string> lines = new List<string> (content.Split ('\n'));
-		int[][] tiles = new int[lines.Count][];
+		List<string> lines = new List<string> (Regex.Split(content, "\r\n"));
+
+		int tileStart = -1;
+		int tileEnd = -1;
+
+		print (lines[0]);
+		print (string.Compare(lines[0], "TILES")); //???????!!!!!!!!!!!!!
+
 		for (int i = 0; i < lines.Count; i++){
-			string[] line = lines[i].Split(' ');
-			tiles[i] = new int[line.Length];
-			for (int j = 0; j < line.Length; j++){
-				tiles[i][j] = int.Parse(line[j]);
+			print (lines[i]);
+			if (string.Compare(lines[i], "TILES") == 0){
+				tileStart = i;
+			}
+			if (lines[i].Length == 0 || (string.Compare(lines[i][0].ToString(), "1") == 0 && string.Compare(lines[i][0].ToString(),"0") == 0)){
+				tileEnd = i;
+				break;
 			}
 		}
-		return tiles;
+
+		if (tileStart != -1){
+			if (tileEnd == -1) tileEnd = lines.Count;
+			int[][] tiles = new int[lines.Count][];
+			for (int i = tileStart; i < tileEnd; i++){
+				if (string.Compare(lines[i],"TILES" ) == 0) {
+					continue;
+				}
+				string[] line = lines[i].Split(' ');
+				tiles[i] = new int[line.Length];
+				for (int j = 0; j < line.Length; j++){
+					tiles[i][j] = int.Parse(line[j]);
+				}
+			}
+			return tiles;
+		}
+		return null;
 	}
 
-	public void WriteTilesToFile(int[][] tiles, string fileName){
+	public void WriteToFile(int[][] tiles, string fileName){
 		string path = "Assets/Resources/LevelFiles/" + fileName + ".txt";
 		StreamWriter sw;
 		if (File.Exists(path)) {
@@ -29,8 +69,8 @@ public class FileIO {
 		else {
 			sw = File.CreateText(path);
 		}
-
-		for (int i = 0; i < tiles.Length; i++){
+		sw.WriteLine("TILES");
+		for (int i = 1; i < tiles.Length; i++){
 			string line = "";
 			for (int j = 0; j < tiles[i].Length; j++){
 				line += tiles[i][j].ToString();
--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -8,23 +8,49 @@ public class LevelEditor : MonoBehaviour {
 	public string inputFile;
 	public string outputFile;
 
+	public Transform wallTile;
+	public Transform floorTile;
+
 	// Use this for initialization
 	void Start () {
-		io = new FileIO();
+		io = gameObject.AddComponent<FileIO>();
 		LoadFile(inputFile);
 		WriteFile(outputFile);
 	}
-	
-	void LoadFile(string fileName){
-		string path = "Assets/Resources/LevelFiles/" + fileName + ".txt";
-		if (fileName != "" && File.Exists(path)) {
-			tiles = io.GetTilesFromFile(fileName);
+
+	// Used by the LoadFile function to add tiles to the screen
+	void LoadLevel(){
+		if (tiles != null){
+			for (int y = 1; y < tiles.Length; y++) {
+				for (int x = 0; x < tiles[y].Length; x++) {
+					if (tiles[y][x] == 0){
+						Instantiate(floorTile, new Vector3(x, y, 1), Quaternion.identity);
+					}
+					else if (tiles[y][x] == 1){
+						Instantiate(wallTile, new Vector3(x, y, 0), Quaternion.identity);
+					}
+					//more types of tiles coming up, e.g. kill zone
+				}
+			}
+		}
+		else {
+			print ("There is no level yet.");		
 		}
 	}
 
+	// Load a level from file
+	void LoadFile(string fileName){
+		string path = "Assets/Resources/LevelFiles/" + fileName + ".txt";
+		if (fileName != "" && File.Exists(path)) {
+			tiles = io.ReadFromFile(fileName);
+		}
+		LoadLevel();
+	}
+
+	// Write a level to file
 	void WriteFile(string fileName){
 		if (fileName != "") {
-			io.WriteTilesToFile(tiles, fileName);
+			io.WriteToFile(tiles, fileName);
 		}
 	}
 
